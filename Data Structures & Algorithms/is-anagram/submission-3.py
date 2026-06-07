a8912f4e00430341141264a28600afe939e8f223
kategaru sri sai krishna <krishnakategaruai@gmail.com>
2026-06-07 14:50:45 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        return sorted(s) == sorted(t)
+        # if len(s)!=len(t):
+        #     return False
+        # from collections import defaultdict
+        # count = defaultdict(int)
+        # for ch in s:
+        #     count[ch] += 1
+        # for ch in t:
+        #     if ch not in count or count[ch] == 0:
+        #         return False
+        #     count[ch]-=1
+        # return True
+        
